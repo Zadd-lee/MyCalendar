8baca294775df5b4763ee447a5a5eb609b34dcc9
이자드 <kimminkyeong0418@gmail.com>
2024-11-07 11:54:00 +0900

    bug: 사용자 검색 기능 버그 fix

--- src/main/java/com/mycalendar/controller/UserController.java
+++ src/main/java/com/mycalendar/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.mycalendar.controller;
 
+import com.mycalendar.model.User;
+import com.mycalendar.model.dto.UserResponseDto;
 import com.mycalendar.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService service;
-/*
     @GetMapping("{id}")
     public ResponseEntity<UserResponseDto> findUserById(@PathVariable(value = "id") String id) {
-        return new ResponseEntity<>(service.findUserById(id), HttpStatus.OK);
-    }*/
+        User user = service.findUserById(id);
+        UserResponseDto userResponseDto = new UserResponseDto(user);
+        return new ResponseEntity<>(userResponseDto, HttpStatus.OK);
+    }
 }
